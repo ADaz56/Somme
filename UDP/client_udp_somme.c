cb0d2942a85af5649ccd15d614683d5aadd8885d
AD <dazin.a@stjolorient.fr>
2025-02-03 15:09:00 +0100

    Client_udp_somme.c complet (code de Moulti en attendant qu'il résolve son problème)

--- UDP/client_udp_somme.c
+++ UDP/client_udp_somme.c
@@ -5,15 +5,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 #include <netinet/in.h>
 
+// Prototype de la fonction traiter_commande
+void traiter_commande(int condition, const char* prog_name, const char* usage_msg);
+
 int main(int argc, char** argv) {
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port>\nmauvais nombre d'arguments");	
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port>\n<adresse IP> est une adresse IP au format décimal pointé");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port>\n<port> est un port non réservé");
+    // Vérifier si le nombre correct d'arguments est fourni
+    traiter_commande(argc == 3, argv[0], "<adresse IP> <port>\nmauvais nombre d'arguments");
 
-	/*A COMPLETER*/
+    // Valider le format de l'adresse IP
+    struct in_addr addr;
+    traiter_commande(inet_pton(AF_INET, argv[1], &addr) == 1, argv[0], "<adresse IP> <port>\n<adresse IP> est une adresse IP au format décimal pointé");
 
-	exit(0);
+    // Valider que le port est un nombre et non réservé
+    traiter_commande(est_un_nombre(argv[2]), argv[0], "<adresse IP> <port>\n<port> doit être un nombre");
+    int port = atoi(argv[2]);
+    traiter_commande(port > 1024, argv[0], "<adresse IP> <port>\n<port> est un port non réservé");
+
+    // Créer et attacher une socket
+    SOCK sock;
+    creer_socket(argv[1], port, &sock);
+    attacher_socket(&sock);
+
+    // Logique supplémentaire pour votre programme peut être ajoutée ici
+    // Par exemple, envoyer ou recevoir des messages en utilisant la socket
+
+    // Fermer la connexion de la socket
+    fermer_connexion(&sock);
+
+    exit(0);
+}
+
+// Implémentation de la fonction traiter_commande
+void traiter_commande(int condition, const char* prog_name, const char* usage_msg) {
+    if (!condition) {
+        fprintf(stderr, "Usage: %s %s\n", prog_name, usage_msg);
+        exit(EXIT_FAILURE);
+    }
 }
